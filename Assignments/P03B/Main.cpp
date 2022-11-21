7fc8d8f91c7e815f0dda455730fb7a403de58c59
RicardoCesarRios <98514621+RicardoCesarRios@users.noreply.github.com>
2022-11-21 02:44:39 -0600

    Update Main.cpp

--- Assignments/P03B/Main.cpp
+++ Assignments/P03B/Main.cpp
@@ -1,3 +1,36 @@
+
+/*****************************************************************************
+*                    
+*  Author:          Ricardo Rios
+*  Email:           RicardoCesarRios14@outlook.com
+*  Label:           P03B
+*  Title:           Bolniverse Fight club
+*  Course:          2143 OOP
+*  Semester:        Fall 2022
+* 
+*  Description:
+*       By way of dynamic polymorphism this program creates a fighter character
+by way of reading in a list of characters from an input file and placing thoses 
+characters into a vector then to a character pointer that points to a base class
+and assigns random attributes withing certain parameters to said character such
+as "1.d.4" or "1.d.6" damage for a warrior class, and that makes the attackers.
+the defenders are generated at a ratio of 1 for every 100 attackers, but have 
+the ability to regen HP. The two sides battle and the winner is decided by which
+side still has characters within their vector "One could say to the last man standing".
+* 
+*  Usage:
+*       Insert a file with a list of attackers 
+* 
+*  Files:           
+main.cpp
+dice.hpp
+fighter.hpp
+helpers.hpp
+weapon.hpp
+
+*****************************************************************************/
+
+
 #include "dice.hpp"
 #include "fighter.hpp"
 #include "helpers.hpp"
@@ -16,13 +49,15 @@ int main() {
   infile.open("attackers.txt");
   outfile.open("results.txt");
   int attackersCount = 0, deffenderscount = 0;
-
+  // taking count of character in order to keep track of how
+  // many of said char to make 
   int WarriorCount = 0;
   int WizardCount = 0;
   int ArcherCount = 0;
   int ElfCount = 0;
   int DragonbornCount = 0;
-
+//while the file is not ended infile character names and add to characters counts
+// character counts determine how many instances of each char to create
   while (!infile.eof()) {
     string fighterType;
     infile >> fighterType;
@@ -46,7 +81,7 @@ int main() {
 
   BaseFighter *CurAttacker;
   BaseFighter *CurDefender;
-
+//pushing instances of characters based on their counts
   for (int i = 0; i < WarriorCount; i++) {
     Attackers.push_back(new Warrior("attacker"));
   }
@@ -65,6 +100,16 @@ int main() {
   for (int i = 0; i < DragonbornCount; i++) {
     Attackers.push_back(new DragonBorn("attacker"));
   }
+// pushing defenders based on the counts of their respective 
+// character into the defender vector
+    for (int i = 0; i < Attackers.size() / 10; i++) {
+    Defenders.push_back(new Warrior("defender"));
+    Defenders.push_back(new Wizard("defender"));
+    Defenders.push_back(new Archer("defender"));
+    Defenders.push_back(new Elf("defender"));
+    Defenders.push_back(new DragonBorn("defender"));
+      } 
+  //attack loop
   while (Attackers.size() && Defenders.size()) {
     for (int i = 0; i < Defenders.size(); i++) {
       Defenders[i]->regen();
@@ -93,15 +138,15 @@ int main() {
       Defenders.pop_back();
     }
   }
-
+    //
   if (Attackers.size() == 0) {
-    cout << "Defenders won!" << '\n'; 
+    cout << "Defenders won!" << endl; 
     }
    else {
-    cout << "Attackers won!" << '\n';
+    cout << "Attackers won!" << endl;
   }
-     outfile << "Defenders alive: " << Defenders.size() << endl;
+   outfile << "Defenders alive: " << Defenders.size() << endl;
   outfile << "Attackers alive: " << Attackers.size() << endl;
    outfile.close();
-  }
+  
   }
